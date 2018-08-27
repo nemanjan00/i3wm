80b70305a532ac59cd0f1cdca65e2b822e9f7b47
nemanjan00 <nemanjan00@gmail.com>
2018-08-27 22:06:23 +0200

    Loop

--- loop.sh
+++ loop.sh
@@ -1,7 +1,6 @@
 while :
 do
-	bash ~/.i3/wallpaper.sh
 	bash ~/.i3/sync.sh
-
 	sleep $((10 * 60))
+	bash ~/.i3/wallpaper.sh
 done
--- wallpaper.sh
+++ wallpaper.sh
@@ -11,23 +11,24 @@ let "number = $LASTNUM % $RANGE"
 echo $number > $WALLPAPERS/.last
 
 DELAY=0.01
+QUALITY=0.3
+RESIZE=50%
 
 echo $#
 
 if [ $# -ne 1 ]; then
-	convert ~/.wallpaper/old.jpg -fill black -blur 10% -fill black -colorize 10% ~/.wallpaper/transition1.jpg
-	convert ~/.wallpaper/old.jpg -fill black -blur 20% -fill black -colorize 20% ~/.wallpaper/transition2.jpg
-	convert ~/.wallpaper/old.jpg -fill black -blur 35% -fill black -colorize 35% ~/.wallpaper/transition3.jpg
-	convert ~/.wallpaper/old.jpg -fill black -blur 50% -fill black -colorize 50% ~/.wallpaper/transition4.jpg
-	convert ~/.wallpaper/old.jpg -fill black -blur 75% -fill black -colorize 75% ~/.wallpaper/transition5.jpg
-	convert ~/.wallpaper/old.jpg -fill black -blur 100% -fill black -colorize 100% ~/.wallpaper/transition6.jpg
+	convert ~/.wallpaper/old.jpg -resize $RESIZE -fill black -blur 10% -fill black -colorize 10% -quality $QUALITY ~/.wallpaper/transition1.jpg
+	convert ~/.wallpaper/old.jpg -resize $RESIZE -fill black -blur 20% -fill black -colorize 20% -quality $QUALITY ~/.wallpaper/transition2.jpg
+	convert ~/.wallpaper/old.jpg -resize $RESIZE -fill black -blur 35% -fill black -colorize 35% -quality $QUALITY ~/.wallpaper/transition3.jpg
+	convert ~/.wallpaper/old.jpg -resize $RESIZE -fill black -blur 50% -fill black -colorize 50% -quality $QUALITY ~/.wallpaper/transition4.jpg
+	convert ~/.wallpaper/old.jpg -resize $RESIZE -fill black -blur 75% -fill black -colorize 75% -quality $QUALITY ~/.wallpaper/transition5.jpg
+	convert ~/.wallpaper/old.jpg -resize $RESIZE -fill black -blur 100% -fill black -colorize 100% -quality $QUALITY ~/.wallpaper/transition6.jpg
 
-	convert $WALLPAPERS/${ALIST[$number]} -fill black -blur 100% -fill black -colorize 100% ~/.wallpaper/transition7.jpg
-	convert $WALLPAPERS/${ALIST[$number]} -fill black -blur 75% -fill black -colorize 75% ~/.wallpaper/transition8.jpg
-	convert $WALLPAPERS/${ALIST[$number]} -fill black -blur 50% -fill black -colorize 50% ~/.wallpaper/transition9.jpg
-	convert $WALLPAPERS/${ALIST[$number]} -fill black -blur 35% -fill black -colorize 35% ~/.wallpaper/transition10.jpg
-	convert $WALLPAPERS/${ALIST[$number]} -fill black -blur 20% -fill black -colorize 20% ~/.wallpaper/transition11.jpg
-	convert $WALLPAPERS/${ALIST[$number]} -fill black -blur 10% -fill black -colorize 10% ~/.wallpaper/transition12.jpg
+	convert $WALLPAPERS/${ALIST[$number]} -resize $RESIZE -fill black -blur 75% -fill black -colorize 75% -quality $QUALITY ~/.wallpaper/transition7.jpg
+	convert $WALLPAPERS/${ALIST[$number]} -resize $RESIZE -fill black -blur 50% -fill black -colorize 50% -quality $QUALITY ~/.wallpaper/transition8.jpg
+	convert $WALLPAPERS/${ALIST[$number]} -resize $RESIZE -fill black -blur 35% -fill black -colorize 35% -quality $QUALITY ~/.wallpaper/transition9.jpg
+	convert $WALLPAPERS/${ALIST[$number]} -resize $RESIZE -fill black -blur 20% -fill black -colorize 20% -quality $QUALITY ~/.wallpaper/transition10.jpg
+	convert $WALLPAPERS/${ALIST[$number]} -resize $RESIZE -fill black -blur 10% -fill black -colorize 10% -quality $QUALITY ~/.wallpaper/transition11.jpg
 
 	feh --bg-fill ~/.wallpaper/transition1.jpg
 	sleep $DELAY
@@ -51,8 +52,6 @@ if [ $# -ne 1 ]; then
 	feh --bg-fill ~/.wallpaper/transition10.jpg
 	sleep $DELAY
 	feh --bg-fill ~/.wallpaper/transition11.jpg
-	sleep $DELAY
-	feh --bg-fill ~/.wallpaper/transition12.jpg
 	sleep $DELAY
 
 	rm ~/.wallpaper/transition*
